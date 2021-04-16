e5ba8d7471eeb416c577e3c1bdb3a7f9aaa5dc22
mrgling <jonas.glingert@medieinstitutet.se>
2021-04-16 15:02:56 +0200

    post

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.get('/api/v1/:id',(req, res) => {
 })
 
 app.post('/api/v1',(req, res) => {
-    req.body
+    req.body.first
+    res.json(req.body.first)
 })
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
