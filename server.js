d3eb548ddfa46085852f6fe041d3056da6498c8f
mrgling <jonas.glingert@medieinstitutet.se>
2021-04-16 14:58:50 +0200

    post

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const port = 3000;
+
 const app = express();
 
 app.use(express.json())
 
 app.get('/api/v1',(req, res) => {
-    res.json("Mitt REST-API V1")
-    
+    res.json("Mitt REST-API V1.0")    
+})
+
+app.get('/api/v1/:id',(req, res) => {
+    req.params.id
+    res.json("GET: " + req.params.id)    
+})
+
+app.post('/api/v1',(req, res) => {
+    req.body
 })
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
